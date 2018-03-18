3d77547c230045ddad894eed12e3021596e9d822
Hector Pulido <Hectorandrespp@gmail.com>
2018-03-17 21:55:47 -0500

    First Commit

---
+++ Twitch Version/Assets/Scripts/Resistance.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Resistance : MonoBehaviour {
+
+	public float resistance;
+	public GameObject marioExplosion;
+
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		print (col.relativeVelocity.magnitude);
+
+		if (col.relativeVelocity.magnitude > resistance) {
+			var m = Instantiate (marioExplosion, 
+				transform.position, 
+				Quaternion.identity);
+			Destroy (m, 5);
+
+			Destroy (gameObject, 0.1f);	
+		} else {
+			resistance -= col.relativeVelocity.magnitude;
+		}
+
+	}
+}
---
+++ Twitch Version/Assets/Scripts/bird.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bird : MonoBehaviour 
+{
+	public Transform anchor;
+	public float springRange;
+	public float maxVel;
+
+	bool canDrag = true;
+	Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () 
+	{
+		rb = GetComponent<Rigidbody2D> ();	
+
+		transform.position = anchor.position;
+	}
+
+
+	Vector3 Dir;
+	void OnMouseDrag()
+	{
+		if (!canDrag)
+			return;
+		var Pos = Camera.main.ScreenToWorldPoint (Input.mousePosition); //Posicion del mouse
+		Dir = Pos - anchor.position; // hacia donde mira pajarito
+
+		Dir.z = 0;
+
+		if (Dir.magnitude > springRange) 
+		{
+			Dir = Dir.normalized * springRange;		
+		}
+
+		transform.position = anchor.position + Dir;
+
+	}
+	void OnMouseUp() 
+	{
+		if (!canDrag)
+			return;
+		canDrag = false;
+
+		rb.bodyType = RigidbodyType2D.Dynamic;
+
+		rb.velocity = - Dir.normalized * maxVel * Dir.magnitude / springRange;
+	}
+		
+}
