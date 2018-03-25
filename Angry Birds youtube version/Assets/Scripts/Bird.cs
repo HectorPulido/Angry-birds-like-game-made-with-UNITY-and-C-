70c82cb66e2644b5d6c2e9f1c77e7212bdb80e02
Hector Pulido <Hectorandrespp@gmail.com>
2018-03-24 20:13:14 -0500

    Youtube version uploaded

---
+++ Angry Birds youtube version/Assets/Scripts/Bird.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+[RequireComponent(typeof(Rigidbody2D))]
+public class Bird : MonoBehaviour 
+{
+	public Transform pivot;
+	public float springRange;
+	public float maxVel;
+
+	Rigidbody2D rb;
+	void Start () 
+	{
+		rb = GetComponent<Rigidbody2D> ();
+		rb.bodyType = RigidbodyType2D.Kinematic;
+	}
+
+	bool canDrag = true;
+	Vector3 dis;
+	void OnMouseDrag()
+	{
+		if (!canDrag)
+			return;
+		
+		var pos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+		dis = pos - pivot.position;
+		dis.z = 0;
+		if (dis.magnitude > springRange) 
+		{
+			dis = dis.normalized * springRange;		
+		}
+		transform.position = dis + pivot.position;
+	}
+
+	void OnMouseUp()
+	{
+		if (!canDrag)
+			return;
+		canDrag = false;
+
+		rb.bodyType = RigidbodyType2D.Dynamic;
+		rb.velocity = -dis.normalized * maxVel * dis.magnitude / springRange;
+	}
+}
---
+++ Angry Birds youtube version/Assets/Scripts/DestructableObject.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestructableObject : MonoBehaviour 
+{
+	public float resistance;
+	public GameObject explosionPrefab;
+
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		if (col.relativeVelocity.magnitude > resistance) {
+		
+			if (explosionPrefab != null) {
+				var go = Instantiate (explosionPrefab, 
+					transform.position, 
+					Quaternion.identity);
+				
+			
+				Destroy (go, 3);
+			}
+
+			Destroy (gameObject, 0.1f);
+
+		} else {
+			resistance -= col.relativeVelocity.magnitude;
+		
+		}
+
+	}
+}
